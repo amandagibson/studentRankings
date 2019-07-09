94c6cdcf74b93d62bb6395fcfe556c17ce026388
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-09 14:49:49 +0200

    adds congratulations messages for students that scored 950 and above

--- rankStudents.py
+++ rankStudents.py
@@ -4,8 +4,8 @@
 # Appreciate students who scored 950 and above.
 import operator
 
-
 def readStudentDetails():
+  print()
   print("Enter the number of students: ")
   numberOfStudents = int(input())
   studentRecord = {}
@@ -15,10 +15,12 @@ def readStudentDetails():
     print("Enter the grade of the student: ")
     grades = int(input())
     studentRecord[name] = grades
+    print()
   return studentRecord
 
 
 def rankStudents(studentRecord):
+  print()
   sortedStudentRecord = sorted(
       studentRecord.items(), key=operator.itemgetter(1), reverse=True)
   print(sortedStudentRecord)
@@ -28,18 +30,28 @@ def rankStudents(studentRecord):
       sortedStudentRecord[1][0], sortedStudentRecord[1][1]))
   print("{} has secured the third highest grade with a score of {}!".format(
       sortedStudentRecord[2][0], sortedStudentRecord[2][1]))
+  print()
   return sortedStudentRecord
 
 
 def rewardStudents(sortedStudentRecord, reward):
+  print()
   print("{} has received a cash reward of ${}".format(sortedStudentRecord[0][0], reward[0]))
   print("{} has received a cash reward of ${}".format(sortedStudentRecord[1][0], reward[1]))
-  print("{} has received a cash reward of ${}".format(sortedStudentRecord[2][1], reward[2]))
+  print("{} has received a cash reward of ${}".format(sortedStudentRecord[2][0], reward[2]))
+  print()
 
-def appreciateStudents():
-  pass
+def appreciateStudents(sortedStudentRecord):
+  print()
+  for record in sortedStudentRecord:
+    if record[1] >= 950:
+      print("Congratulations on scoring {} marks, {}!".format(record[1], record[0]))
+    else:
+      break
+  print()
 
 studentRecord = readStudentDetails()
 sortedStudentRecord = rankStudents(studentRecord)
 reward = (500, 300, 100)
 rewardStudents(sortedStudentRecord, reward)
+appreciateStudents(sortedStudentRecord)
