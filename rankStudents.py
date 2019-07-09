1b2272c699cc9c61a23ae2d6ede5249addbf96d6
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-09 15:15:39 +0200

    removes line breaks

--- rankStudents.py
+++ rankStudents.py
@@ -14,21 +14,15 @@ def readStudentDetails():
     print()
   return studentRecord
 
-
 def rankStudents(studentRecord):
   print()
-  sortedStudentRecord = sorted(
-      studentRecord.items(), key=operator.itemgetter(1), reverse=True)
+  sortedStudentRecord = sorted(studentRecord.items(), key=operator.itemgetter(1), reverse=True)
   print(sortedStudentRecord)
-  print("{} has secured the highest grade with a score of {}!".format(
-      sortedStudentRecord[0][0], sortedStudentRecord[0][1]))
-  print("{} has secured the second highest grade with a score of {}!".format(
-      sortedStudentRecord[1][0], sortedStudentRecord[1][1]))
-  print("{} has secured the third highest grade with a score of {}!".format(
-      sortedStudentRecord[2][0], sortedStudentRecord[2][1]))
+  print("{} has secured the highest grade with a score of {}!".format(sortedStudentRecord[0][0], sortedStudentRecord[0][1]))
+  print("{} has secured the second highest grade with a score of {}!".format(sortedStudentRecord[1][0], sortedStudentRecord[1][1]))
+  print("{} has secured the third highest grade with a score of {}!".format(sortedStudentRecord[2][0], sortedStudentRecord[2][1]))
   print()
   return sortedStudentRecord
-
 
 def rewardStudents(sortedStudentRecord, reward):
   print()
