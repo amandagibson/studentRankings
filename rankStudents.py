f1d2f3d282db7039ad558709597574c0ea85e535
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-09 13:16:16 +0200

    saves student name and grade into a dictionary, adds operator to sort dictionary into a tuple in descending order and prints out the top 3 students and grades.

---
+++ rankStudents.py
@@ -0,0 +1,39 @@
+#Read the names and grades of at least 3 students.
+#Rank the top 3 students with highest marks.
+#Give cash rewards. $500 for first, $300 for second and $100 for third rank. These amounts cannot be modified.
+#Appreciate students who scored 950 and above.
+
+import operator
+
+def readStudentDetails():
+  print("Enter the number of students: ")
+  numberOfStudents = int(input())
+  studentRecord = {}
+  for student in range(0, numberOfStudents):
+    print("Enter the name of the student: ")
+    name = input()
+    print("Enter the grade of the student: ")
+    grades = int(input())
+    studentRecord[name] = grades
+  return studentRecord
+
+
+
+def rankStudents(studentRecord):
+  sortedStudentRecord = sorted(studentRecord.items(), key = operator.itemgetter(1), reverse = True)
+  print(sortedStudentRecord)
+  print("{} has secured the highest grade with a score of {}!".format(sortedStudentRecord[0][0], sortedStudentRecord[0][1]))
+  print("{} has secured the second highest grade with a score of {}!".format(sortedStudentRecord[1][0], sortedStudentRecord[1][1]))
+  print("{} has secured the third highest grade with a score of {}!".format(sortedStudentRecord[2][0], sortedStudentRecord[2][1]))
+
+
+
+def rewardStudents():
+  pass
+
+def appreciateStudents():
+  pass
+
+
+studentRecord = readStudentDetails()
+rankStudents(studentRecord)
